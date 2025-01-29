c64700d0426a1a79833bc0134377a5020809951d
cristianmfr <cristian@r1engenharia.com.br>
2025-01-29 11:46:17 -0300

    remove firebase logic and enable introspection

--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -12,6 +12,7 @@ export class GqlConfigService implements GqlOptionsFactory {
       autoSchemaFile: join(process.cwd(), 'src/shared/graphql/schema.gql'),
       playground: false,
       installSubscriptionHandlers: true,
+      introspection: true,
       context: (ctx) => {
         return ctx;
       },
